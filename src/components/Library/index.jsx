7d27bd5993871205032b97133431652561ca7b00
Ismael López <jackzepol@outlook.com>
2026-09-07 08:31:13 -0600

    Refactorización usando Redux

--- src/components/Library/index.jsx
+++ src/components/Library/index.jsx
@@ -38,32 +38,3 @@ const Library = () => {
 };
 
 export default Library;
-
-/*
-import React from "react";
-import Song from "../Song/index";
-import { LibraryContainer, Title, EmptyMessage } from "./styles";
-
-
-const Library = ({ songs, onRemoveSong }) => {
-  return (
-    <LibraryContainer>
-      <Title>Mi biblioteca</Title>
-
-      {songs.length === 0 && <EmptyMessage className="message">No has agregado canciones aún.</EmptyMessage>}
-
-      {songs.map((song) => (
-        <Song
-          key={song.idTrack || Math.random()}
-          title={song.strTrack}
-          artist={song.strArtist}
-          duration={song.intDuration}
-          onRemove={() => onRemoveSong(song.idTrack)}
-        />
-      ))}
-    </LibraryContainer>
-  );
-}
-
-export default Library;
-*/
